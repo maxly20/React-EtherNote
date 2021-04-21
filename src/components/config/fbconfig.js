e137f0c34313189168ac5faca097597c459a0113
maxly20 <maxly2011@gmail.com>
2021-04-21 16:27:45 +1000

    setting up firebase

---
+++ src/components/config/fbconfig.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyDwpiC3I_ChtZNf7LbnQ5jHZsQcN7hzbHQ',
+  authDomain: 'ethernote-eae66.firebaseapp.com',
+  projectId: 'ethernote-eae66',
+  storageBucket: 'ethernote-eae66.appspot.com',
+  messagingSenderId: '251664806071',
+  appId: '1:251664806071:web:fa5260320171e652bfdd32',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
