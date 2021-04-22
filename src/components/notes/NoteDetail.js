92fe5e8858e0ae35ef1683be2858f4072c09c428
maxly20 <maxly2011@gmail.com>
2021-04-22 12:45:40 +1000

    building single note page

--- src/components/notes/NoteDetail.js
+++ src/components/notes/NoteDetail.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase';
+import moment from 'moment';
 
-const NoteDetail = () => {
-    return (
-        <div>
-            
+const NoteDetail = props => {
+  const id = props.match.params.id;
+  useFirestoreConnect([{ collection: 'notes', doc: id }]);
+  const note = useSelector(
+    ({ firestore: { data } }) => data.notes && data.notes[id]
+  );
+  return (
+    <div className='detail__container'>
+      <article className='card'>
+        <div className='card__content'>
+          <span className='card__title'>{note?.title}</span>
+          <p className='card__note-content'>{note?.content}</p>
+          <div>{moment(note?.createdAt.toDate()).calendar()}</div>
         </div>
-    )
-}
+      </article>
+    </div>
+  );
+};
 
-export default NoteDetail
+export default NoteDetail;
